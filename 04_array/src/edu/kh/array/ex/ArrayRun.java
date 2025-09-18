3c40e1620c9edeb385fa99edab53d1033210c51f
khcmh2509 <khcmh2509@gmail.com>
2025-09-18 17:00:42 +0900

    배열 예제 끝

--- 04_array/src/edu/kh/array/ex/ArrayRun.java
+++ 04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -12,6 +12,12 @@ public class ArrayRun {
 		//arrayEx1.ex6();
 		//arrayEx1.ex7();
 		//arrayEx1.ex8();
+		
+		ArrayExample2 arrayEx2 = new ArrayExample2();
+		//arrayEx2.shallowCopy();
+		//arrayEx2.deepCopy();
+		arrayEx2.createLottoNumber();
+		
 	}
 
 }
