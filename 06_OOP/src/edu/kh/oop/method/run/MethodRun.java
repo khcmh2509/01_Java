4f06460d3ef39f8142c320a0aa80f461cf04223d
khcmh2509 <khcmh2509@gmail.com>
2025-09-24 11:33:41 +0900

    메서드 까지

--- 06_OOP/src/edu/kh/oop/method/run/MethodRun.java
+++ 06_OOP/src/edu/kh/oop/method/run/MethodRun.java
@@ -1,6 +1,8 @@
 package edu.kh.oop.method.run;
 
+import edu.kh.oop.method.model.service.MemberService;
 import edu.kh.oop.method.model.service.MethodExample;
+import edu.kh.oop.method.model.vo.Member;
 
 public class MethodRun {
 
@@ -8,6 +10,15 @@ public class MethodRun {
 		MethodExample ex = new MethodExample();
 		//ex.method1();
 
+		MemberService service = new MemberService();
+		//service.displayMenu();
+		
+		Member member = new Member("user01", "pass01", 
+									"홍길동", 20);
+		
+		System.out.println(member);
+		System.out.println(member.toString());
+		
 	}
 
 }
