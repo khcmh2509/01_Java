59b672a0f379353e7a8f3edba1eab549526b2dde
khcmh2509 <khcmh2509@gmail.com>
2025-10-01 10:51:19 +0900

    학생정보관리프로그램 검색까지

--- 11_collection/src/edu/kh/collection/pack1/model/service/StudentService.java
+++ 11_collection/src/edu/kh/collection/pack1/model/service/StudentService.java
@@ -28,6 +28,15 @@ public class StudentService {
 	
 	private Scanner sc = new Scanner(System.in);
 	
+	public StudentService() { // 기본생성자
+		studentList.add(new Student("홍길동", 23, "서울시 중구", 'M', 100));
+		studentList.add(new Student("홍길동", 20, "경기도 안산", 'F', 100));
+		studentList.add(new Student("강하늘", 25, "서울시 강남구", 'M', 80));
+		studentList.add(new Student("오미나", 24, "충북 청주시", 'F', 90));
+		studentList.add(new Student("박주희", 24, "서울시 서대문구", 'F', 70));
+	}
+	
+	
 	// new ArrayList() : 기본생성자 -> 기본 크기 10짜리 리스트 생성
 	// 하지만 리스트의 크기는 늘었다 줄었다 하기때문에 큰 의미는 없음
 	public void ex() { // List 테스트용 메서드
@@ -77,6 +86,8 @@ public class StudentService {
 				System.out.println("4. 학생 정보 제거");
 				System.out.println("5. 이름으로 검색(완전일치)");
 				System.out.println("6. 이름으로 검색(포함)");
+				System.out.println("7. 나이순으로 정렬"); // Comparable 사용
+				System.out.println("8. 이름순으로 정렬"); // Comparator 사용
 				System.out.println("0. 프로그램 종료");
 				
 				System.out.print("메뉴 번호 선택 : ");
@@ -85,11 +96,13 @@ public class StudentService {
 				
 				switch (menuNum) {
 				case 1: System.out.println( addStudent() ); break;
-				case 2: /* selectAll(); */ break;
-				case 3: /* updateStudent(); */ break;
-				case 4: /* removeStudent(); */ break;
-				case 5: /* searchName1(); */ break;
-				case 6: /* searchName2(); */ break;
+				case 2: selectAll(); break;
+				case 3: System.out.println( updateStudent() ); break;
+				case 4: System.out.println( removeStudent() ); break;
+				case 5: searchName1(); break;
+				case 6: searchName2(); break;
+				case 7 : sortByAge(); break;
+				case 8 : sortByName(); break;
 				case 0: System.out.println("프로그램 종료..."); break;
  				default: System.out.println("메뉴에 작성된 번호만 입력하세요!");
 				}
@@ -113,6 +126,12 @@ public class StudentService {
 	}
 	
 	
+	private void sortByAge() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	/** 
 	 * 1. 학생 정보 추가 메서드
 	 * 
@@ -166,7 +185,234 @@ public class StudentService {
 		
 	}
 	
+	/**
+	 * 2. 학생 전체 조회 메서드
+	 * 
+	 * - List가 비어있는 경우 "학생 정보가 없습니다" 출력
+	 * - 있는 경우 전체 학생 출력
+	 * 
+	 */
+	public void selectAll() {
+		System.out.println("========학생 전체 조회========");
+		
+		// studentList가 비어있는지 확인
+		// List에 저장된 데이터의 개수를 얻어오는 방법 : int List.size()
+		// boolean List.isEmpty() : 컬렉션이 비어있다면 true 반환
+		
+		// if(studentList.size() == 0)
+		if(studentList.isEmpty()) {
+			System.out.println("학생 정보가 없습니다");
+			return; // 현재 메서드를 종료하고 호출한곳으로 돌아감.
+		}
+		
+		// 반복문을 이용하여 studentList에 있는 전체 학생 정보 출력
+		// 일반 for문
+		/*
+		for(int i=0; i < studentList.size(); i++) { // length(X)
+			System.out.println( studentList.get(i) );
+		}
+		*/
+		
+		// 향상된 for문
+		// - 컬렉션, 배열의 모든 요소를 순차적으로 
+		// 반복접근 할 수 있는 for문
+		int index = 0;
+		for(Student std : studentList) {
+			System.out.print((index++) + "번 : ");
+			System.out.println(std);
+		}
+		
+	}
 	
+	
+	/** 
+	 * 3. 학생 정보 수정 메서드
+	 * 
+	 * - 학생 정보가 studentList에 하나라도 있는지 검사,
+	 * 없다면 "입력된 학생정보가 없습니다" 문자열 반환
+	 * 
+	 * - 인덱스 번호로 선택하여 학생을 수정
+	 * 입력된 숫자가 0보다 작은지 검사,
+	 * 작다면 "음수는 입력할 수 없습니다" 반환
+	 * 
+	 * - studentList 범위 내 인덱스번호인지 검사, 
+	 * 이상이라면 "범위를 넘어선 값을 입력할 수 없습니다" 반환
+	 * 
+	 * - 수정할 때
+	 * 1. index 번째에 저장된 학생정보를
+	 * 수정 전에 한번 출력
+	 * 
+	 * 2. 이름, 나이, 사는곳, 성별, 점수 수정할 값 입력받기
+	 * 
+	 * 3. 수정이 완료되었다면, "000의 정보가 변경되었습니다." 반환
+	 * @return
+	 */
+	public String updateStudent() {
+		
+		System.out.println("=======학생 정보 수정========");
+		
+		if(studentList.isEmpty()) {
+			return "입력된 학생정보가 없습니다";
+		}
+		
+		System.out.print("인덱스 번호 : ");
+		int index = sc.nextInt();
+		
+		// 1) 입력된 숫자가 0보다 작은지
+		if(index < 0) return "음수는 입력할 수 없습니다";
+		
+		// 2) studentList 범위 내 인덱스번호인지 검사
+		if(index >= studentList.size()) 
+			return "범위를 넘어선 값을 입력할 수 없습니다";
+		
+		// 수정 코드 작성
+		// 1. index 번째에 저장된 학생정보 출력
+		System.out.println(index + "번째에 저장된 학생 정보");
+		System.out.println(studentList.get(index));
+		
+		// 2. 수정할 내용 입력받기
+		System.out.print("이름 : ");
+		String name = sc.next();
+		
+		System.out.print("나이 : ");
+		int age = sc.nextInt();
+		sc.nextLine(); // 입력버퍼 개행문자 제거
+		
+		System.out.print("사는 곳 : ");
+		String region = sc.nextLine();
+		
+		System.out.print("성별(M/F) : ");
+		char gender = sc.next().charAt(0);
+		
+		System.out.print("점수 : ");
+		int score = sc.nextInt();
+		
+		// 입력받은 index 번째에 수정할 학생정보를 세팅 -> 수정
+		Student temp = studentList.set(index, 
+				new Student(name, age, region, gender, score));
+		// E List.set(int index, E e) 
+		// : 주어진 index번째에 주어진 객체로 덮어쓰기(수정함)
+		// 기존의 객체를 반환함
+		
+		return temp.getName() + "의 정보가 변경되었습니다.";
+		
+	}
+	
+	
+	/**
+	 * 4. 학생 정보 제거 메서드
+	 * 
+	 * 수정과 같은 검사 후 
+	 * 삭제 시 "정말 삭제하시겠습니까? (Y/N) : "
+	 * Y를 입력했다면 해당 인덱스에 있는 객체 삭제 후
+	 * "000님의 정보가 제거되었습니다" 문자열 반환(리턴)
+	 * N을 입력했다면 "취소" 리턴
+	 * 
+	 */
+	public String removeStudent() {
+		
+		System.out.println("======학생 정보 제거========");
+		
+		if(studentList.isEmpty()) {
+			return "입력된 학생정보가 없습니다";
+		}
+		
+		System.out.print("인덱스 번호 : ");
+		int index = sc.nextInt();
+		
+		// 1) 입력된 숫자가 0보다 작은지
+		if(index < 0) return "음수는 입력할 수 없습니다";
+		
+		// 2) studentList 범위 내 인덱스번호인지 검사
+		if(index >= studentList.size()) 
+			return "범위를 넘어선 값을 입력할 수 없습니다";
+		
+		// 삭제
+		System.out.print("정말 삭제 하시겠습니까? (Y/N) : ");
+		char ch = sc.next().toUpperCase().charAt(0);
+		//		"y" -> "Y" -> 'Y'
+		
+		// E List.remove(int index) 
+		// : 리스트에서 index번째 요소를 제거
+		// 이 때 제거된 요소가 반환됨.
+		// * List 중간에 비어있는 인덱스가 없게 하기 위해서
+		// remove() 동작 시 뒤쪽 요소를 한칸씩 
+		// 자동으로 당겨오는 행위하고 있음
+		if(ch == 'Y') { 
+			Student temp = studentList.remove(index);
+			return temp.getName() + "의 정보가 제거되었습니다.";
+		}
+		
+		return "취소";
+		
+	}
+	
+	
+	/**
+	 * 5. 이름이 일치하는 학생을 찾아 조회하는 메서드(완전 일치)
+	 * 
+	 * - 검색할 이름을 입력받아 studentList에서 꺼내온
+	 * Student 객체의 name값이 같은지 비교
+	 * 
+	 * - 일치하는 경우 Student 객체 출력 
+	 * - 일치하는게 없다면 "검색 결과가 없습니다" 출력
+	 * 
+	 */
+	public void searchName1() {
+		
+		System.out.println("====학생 검색(이름 완전 일치)====");
+	
+		System.out.print("검색할 이름 입력 : ");
+		String input = sc.next();
+		
+		boolean flag = true;
+		
+		// 향상된 for문
+		for(Student std : studentList) {
+			
+			if(input.equals(std.getName())) {
+				System.out.println(std);
+				flag = false; 
+			}
+			
+		}
+	
+		if(flag) {
+			System.out.println("검색 결과가 없습니다");
+		}
+		
+		
+	}
+	
+	/**
+	 * 6. 이름에 특정 문자열이 포함되는 학생을 찾아서 조회하는 메서드
+	 * 
+	 * 
+	 */
+	public void searchName2() {
+		
+		System.out.println("====학생 검색(이름 부분 포함)====");
+		
+		System.out.print("이름에 포함되는 문자열 입력 : ");
+		String input = sc.next();
+		
+		boolean flag = true;
+		
+		for(Student std : studentList) {
+			// 순서 유의!!!!
+			// boolean String.contains(문자열) 
+			// String에 문자열이 포함되어있으면 true/false
+			if(std.getName().contains(input)) {
+				// std.getName()에 input이 포함되어있느냐?
+				System.out.println(std);
+				flag = false;
+			}
+		}
+		
+		if(flag) System.out.println("검색 결과가 없습니다");
+		
+		
+	}
 	
 	
 	
